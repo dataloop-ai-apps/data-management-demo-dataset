bd26deeca380324e1a6ccd94fec6a4e4c1e59215
RoniDataloopAi <roni.a@dataloop.ai>
2026-04-29 10:01:46 +0300

    new zip

--- dpks/dataset_contracts_caud/dataset_loader.py
+++ dpks/dataset_contracts_caud/dataset_loader.py
@@ -6,6 +6,5 @@ class DatasetExample(BaseDatasetLoader):
     CAUD Contracts dataset - items.
     """
 
-    zip_url = 'https://storage.googleapis.com/model-mgmt-snapshots/datasets-caud-contracts/caud_contracts.zip'
-    items_path = 'caud_contracts/items/'
-    
+    zip_url = 'https://storage.googleapis.com/model-mgmt-snapshots/datasets-caud-contracts/caud_contracts_pdf.zip'
+    items_path = 'caud_contracts_pdf/items/'
